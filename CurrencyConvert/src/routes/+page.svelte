7322e4b08ac5c99ebaa0cdbba5fc9e2418769382
Poncholo Riego de Dios <choloriegodedios@gmail.com>
2025-01-24 23:20:16 +0800

    Scaffolding

---
+++ CurrencyConvert/src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+	// Add any JavaScript logic here if needed
+</script>
+
+<div class="flex min-h-screen flex-col items-center bg-slate-500">
+
+	<button class="border border-white rounded-xl px-2 pt-2 mb-8 mt-8 group hover:bg-green-300 hover:bg-opacity-40 transition-all hover:-translate-y-1" title="Click to clear all">
+		<p class="mb-4 text-5xl text-white group group-hover:text-black">CURRENCY CONVERSION</p>
+    </button>
+
+	<div class="rounded-xl border border-white p-40">
+		<input type="text" class="p-2" placeholder="Enter value" />
+	</div>
+
+    <select name="Currency" class="bg-transparent border border-b-white w-auto text-white">
+        
+    </select>
+
+</div>
