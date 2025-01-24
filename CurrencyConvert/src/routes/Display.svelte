85a9b5ef461ad3e3cdcf7a4aa1e3668d6e9f5415
Poncholo Riego de Dios <choloriegodedios@gmail.com>
2025-01-25 00:40:03 +0800

    Display and Header
    Created the static parts of the system

---
+++ CurrencyConvert/src/routes/Display.svelte
@@ -0,0 +1,33 @@
+<div
+	class="ml-16 flex h-[26rem] w-[25rem] flex-col overflow-hidden rounded-br-[5rem] rounded-tl-[5rem] bg-blue-500"
+>
+	<div
+		class="flex h-[5rem] w-full items-center justify-end bg-blue-900 px-8 text-4xl italic text-white"
+	>
+		Exchange Rates
+	</div>
+
+	<div class="flex h-1/4 w-full flex-row items-center justify-center gap-8 px-8">
+		<div class="h-1/2 w-1/4 bg-green-500"></div>
+		<div class="h-1/6 w-1/4 text-white text-3xl items-center justify-center flex">USD $</div>
+		<div class="h-1/6 w-1/4 text-white text-3xl items-center justify-center flex">54.20</div>	
+	</div>
+
+	<div class="flex h-1/4 w-full flex-row items-center justify-center gap-8 px-8">
+		<div class="h-1/2 w-1/4 bg-green-500"></div>
+		<div class="h-1/6 w-1/4 text-white text-3xl items-center justify-center flex">USD $</div>
+		<div class="h-1/6 w-1/4 text-white text-3xl items-center justify-center flex">54.20</div>	
+	</div>
+
+	<div class="flex h-1/4 w-full flex-row items-center justify-center gap-8 px-8">
+		<div class="h-1/2 w-1/4 bg-green-500"></div>
+		<div class="h-1/6 w-1/4 text-white text-3xl items-center justify-center flex">USD $</div>
+		<div class="h-1/6 w-1/4 text-white text-3xl items-center justify-center flex">54.20</div>	
+	</div>
+
+	<div class="flex h-1/4 w-full flex-row items-center justify-center gap-8 px-8">
+		<div class="h-1/2 w-1/4 bg-green-500"></div>
+		<div class="h-1/6 w-1/4 text-white text-3xl items-center justify-center flex">USD $</div>
+		<div class="h-1/6 w-1/4 text-white text-3xl items-center justify-center flex">54.20</div>	
+	</div>
+</div>
---
+++ CurrencyConvert/src/routes/Header.svelte
@@ -0,0 +1,12 @@
+<div class="flex">
+	<div class="h-[6.5rem] w-[45rem] ml-20 text-7xl text-white bg-blue-900 rounded-tl-3xl -skew-x-[15deg]">
+		<div class="h-[5.7rem] w-[42.5rem] text-7xl text-white bg-blue-700 rounded-tl-3xl">
+            <div class="h-[4.8rem] w-[40rem] px-6 text-7xl text-white bg-blue-500 rounded-tl-3xl pl-20 pb-4 font-bold">
+                Current Sea
+            </div>
+        </div>
+	</div>
+	<!-- <div class="h-full w-auto text-3xl text-white items-center flex">
+		A currency exchange web application
+	</div> -->
+</div>
