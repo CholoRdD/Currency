0facfe1c7d9d20db65e64e79dbc1a5588eb8f556
Poncholo Riego de Dios <choloriegodedios@gmail.com>
2025-01-27 00:59:30 +0800

    Creating an array for all currencies

--- CurrencyConvert/src/routes/CurrencyList.svelte
+++ CurrencyConvert/src/routes/CurrencyList.svelte
@@ -1,24 +1,26 @@
+<script>
+
+	let apiKey = 'cfa119cbaabf69fb4a42fdc1';
+	let api = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD`;
+	import { currency } from './CurrencyArray.js';
+    let title = "";
+
+function titleChange(code) {
+ title = code;
+};
+
+
+</script>
+
 <div>
-    <div class="w-64 h-56 text-white bg-transparent gap-1 flex-wrap border-r-2 border-green-900 overflow-scroll overflow-x-hidden flex items-center justify-center rounded-2xl">
-<button class="w-full h-10 hover:bg-green-600 hover:font-bold border-blue-800">RAWR</button>
-<button class="w-full h-10 hover:bg-green-600 hover:font-bold border-blue-800">RAWR</button>
-<button class="w-full h-10 hover:bg-green-600 hover:font-bold border-blue-800">RAWR</button>
-<button class="w-full h-10 hover:bg-green-600 hover:font-bold border-blue-800">RAWR</button>
-<button class="w-full h-10 hover:bg-green-600 hover:font-bold border-blue-800">RAWR</button>
-<button class="w-full h-10 hover:bg-green-600 hover:font-bold border-blue-800">RAWR</button>
-<button class="w-full h-10 hover:bg-green-600 hover:font-bold border-blue-800">RAWR</button>
-<button class="w-full h-10 hover:bg-green-600 hover:font-bold border-blue-800">RAWR</button>
-<button class="w-full h-10 hover:bg-green-600 hover:font-bold border-blue-800">RAWR</button>
-<button class="w-full h-10 hover:bg-green-600 hover:font-bold border-blue-800">RAWR</button>
-<button class="w-full h-10 hover:bg-green-600 hover:font-bold border-blue-800">RAWR</button>
-<button class="w-full h-10 hover:bg-green-600 hover:font-bold border-blue-800">RAWR</button>
-<button class="w-full h-10 hover:bg-green-600 hover:font-bold border-blue-800">RAWR</button>
-<button class="w-full h-10 hover:bg-green-600 hover:font-bold border-blue-800">RAWR</button>
-<button class="w-full h-10 hover:bg-green-600 hover:font-bold border-blue-800">RAWR</button>
-<button class="w-full h-10 hover:bg-green-600 hover:font-bold border-blue-800">RAWR</button>
-<button class="w-full h-10 hover:bg-green-600 hover:font-bold border-blue-800">RAWR</button>
-<button class="w-full h-10 hover:bg-green-600 hover:font-bold border-blue-800">RAWR</button>
 
+	<div
+		class="flex h-56 w-64 flex-wrap items-center justify-center gap-1
+        overflow-scroll overflow-x-hidden rounded-2xl border-r-2 border-green-900 bg-transparent text-white"
+	>
+    {#each currency as code}
+        <button class="h-10 w-full border-blue-800 hover:bg-green-600 hover:font-bold">{code}</button>
 
-    </div>
+    {/each}
+	</div>
 </div>
